13c65eef1d6fb5539ad181ca21f96fe174b69761
Theresa Heine <theresaheine@tutanota.com>
2022-01-15 23:46:59 +0100

    add csvReader and test

---
+++ src/main/java/garden/sudoku/CsvReader.java
@@ -0,0 +1,47 @@
+package garden.sudoku;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+public class CsvReader {
+
+	public List<String[]> readCsvFromRelativePath(String filePath) {
+		List<String[]> csvList = new ArrayList<String[]>();
+		try {
+			Path absolutePath = Paths.get(ClassLoader.getSystemResource(filePath).toURI());
+			csvList = readCsvFromAbsolutePath(absolutePath);
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		return csvList;
+	}
+	
+	private List<String[]> readCsvFromAbsolutePath(Path absolutePath) {
+	    List<String[]> list = new ArrayList<String[]>();
+	    try {
+	    	Reader reader = Files.newBufferedReader(absolutePath);
+			CSVReader csvReader = new CSVReader(reader);
+			list = csvReader.readAll();
+			reader.close();
+			csvReader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CsvException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return list;
+	}
+
+}
--- src/test/java/garden/sudoku/CsvReaderTest.java
+++ src/test/java/garden/sudoku/CsvReaderTest.java
@@ -1,63 +1,40 @@
 package garden.sudoku;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.opencsv.CSVReader;
 
 public class CsvReaderTest {
 	
 	List<Plant> plantList = new ArrayList<Plant>();
 	
-	@Test
-	public void fillBedOfSize1WithFirstElement() {
-		//givenACsvPath("src/test/resources/simplePlantList.csv");
-		doCsvReader("src/test/resources/simplePlantList.csv");
-		List<Plant> expectedPlantsList = null;
-		Assert.assertEquals(expectedPlantsList, plantList);
-	}
-
-	private List<String> givenACsvPath(String fileName) {
-		List<String> records = new ArrayList<String>();
-		try {
-			File file = new File(fileName);
-			Scanner scanner = new Scanner(file);
-			while (scanner.hasNextLine()) {
-				records.add(mapToPlants(scanner.nextLine()));
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return records;	
-	}
 	
-	private List<List<String>> doCsvReader(String fileName) {
-		List<List<String>> records = new ArrayList<List<String>>();
-		try {
-			FileReader fileReader = new FileReader(fileName);
-			CSVReader csvReader = new CSVReader(fileReader);
-		    String[] values = null;
-		    while ((values = csvReader.readNext()) != null) {
-		        records.add(Arrays.asList(values));
-		    }
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return records;
+	@Test
+	public void readCommaSeperatedValuesFromCsvTest() {
+		List<String[]> csvList = doReadCsvFromRelativPath("simplePlantList.csv");
+		String[] firstLine = {"Pflanzenname", "Gute Nachbarn"};
+		String[] secondLine = {"A", "B"};
+		String[] thirdLine = {"B", "A"};
+		
+		List<String[]> expectedCsvList = new ArrayList<String[]>();
+		expectedCsvList.add(firstLine);
+		expectedCsvList.add(secondLine);
+		expectedCsvList.add(thirdLine);
+		
+		Assert.assertEquals(expectedCsvList.size(), csvList.size());
+		Assert.assertEquals(expectedCsvList.get(0), csvList.get(0));
+		Assert.assertEquals(expectedCsvList.get(1), csvList.get(1));
+		Assert.assertEquals(expectedCsvList.get(2), csvList.get(2));
 	}
 
-	private String mapToPlants(String nextLine) {
-		
-		return nextLine;
+
+	private List<String[]> doReadCsvFromRelativPath(String filePath) {
+		CsvReader csvReader = new CsvReader();
+		return csvReader.readCsvFromRelativePath(filePath);
+			
 	}
+
 
 }
