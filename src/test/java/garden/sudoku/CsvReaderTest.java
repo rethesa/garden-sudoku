da11b6ff9c237fcfce9f303ab3d94d2a9427e34e
Theresa Heine <theresaheine@tutanota.com>
2022-01-27 23:18:06 +0100

    improve test to read csv file

--- src/test/java/garden/sudoku/CsvReaderTest.java
+++ src/test/java/garden/sudoku/CsvReaderTest.java
@@ -10,31 +10,24 @@ public class CsvReaderTest {
 	
 	List<Plant> plantList = new ArrayList<Plant>();
 	
-	
 	@Test
 	public void readCommaSeperatedValuesFromCsvTest() {
 		List<String[]> csvList = doReadCsvFromRelativPath("simplePlantList.csv");
 		String[] firstLine = {"Pflanzenname", "Gute Nachbarn"};
 		String[] secondLine = {"A", "B"};
-		String[] thirdLine = {"B", "A"};
+		String[] thirdLine = {"B", "A, C"};
 		
 		List<String[]> expectedCsvList = new ArrayList<String[]>();
 		expectedCsvList.add(firstLine);
 		expectedCsvList.add(secondLine);
 		expectedCsvList.add(thirdLine);
 		
-		Assert.assertEquals(expectedCsvList.size(), csvList.size());
-		Assert.assertEquals(expectedCsvList.get(0), csvList.get(0));
-		Assert.assertEquals(expectedCsvList.get(1), csvList.get(1));
-		Assert.assertEquals(expectedCsvList.get(2), csvList.get(2));
+		Assert.assertArrayEquals(expectedCsvList.toArray(), csvList.toArray());
 	}
-
 
 	private List<String[]> doReadCsvFromRelativPath(String filePath) {
 		CsvReader csvReader = new CsvReader();
-		return csvReader.readCsvFromRelativePath(filePath);
-			
+		return csvReader.readCsvFromRelativePath(filePath);	
 	}
-
 
 }
